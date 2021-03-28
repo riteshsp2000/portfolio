bef8c3e1cb17a2bf47b6315cfcffd9a25654b3bb
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-28 16:15:45 +0530

    Create Global Styles

---
+++ src/theming/GlobalStyles.tsx
@@ -0,0 +1,94 @@
+// Libraries
+import {createGlobalStyle} from 'styled-components';
+
+// Fonts
+import wotfardLight from '../fonts/Wotfard-Light.ttf';
+import wotfardMedium from '../fonts/Wotfard-Medium.ttf';
+import wotfardRegular from '../fonts/Wotfard-Regular.ttf';
+
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Wotfard';
+    src: url(${wotfardLight}) format('ttf');
+    font-weight: 300;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Wotfard';
+    src: url(${wotfardRegular}) format('ttf');
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: 'Wotfard';
+    src: url(${wotfardMedium}) format('ttf');
+    font-weight: 500;
+    font-style: normal;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  html, body, div,
+  input, button, select, option,
+  h1, h2, h3, h4, h5, h6, p,
+  text {
+    font-family: 'Wotfard';
+  }
+
+  html, body {
+    max-width: 100vw;
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+  }
+
+  /* HTML5 display-role reset for older browsers */
+  article, aside, details, figcaption, figure,
+  footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  body {
+    line-height: 1;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  blockquote, q {
+    quotes: none;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: '';
+    content: none;
+  }
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+`;
+
+export default GlobalStyles;
