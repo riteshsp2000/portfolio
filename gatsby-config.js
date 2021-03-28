0869fffdfa5dc3513314fd7a372c0b70c61b7679
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-28 13:21:28 +0530

    Add support for google fonts, styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-useless-escape */
 module.exports = {
   /* Your site config here */
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,6 +24,30 @@ module.exports = {
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.md`, `.mdx`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1200,
+            },
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `poppins\:300,400,500,600,700`,
+          `muli\:400,400i,500,500i,600,600i,700,700i`,
+        ],
+        display: 'swap',
       },
     },
   ],
