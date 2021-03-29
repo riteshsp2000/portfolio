68b45de67a076197e5adbb80907834e60dab80a2
Ritesh Patil <riteshsp2000@gmail.com>
2021-03-29 10:49:12 +0530

    Update favicon, title and add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,32 @@
 /* eslint-disable no-useless-escape */
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Ritesh Patil',
+    description:
+      'Ritesh Patil is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://riteshpatil.com',
+    // image: '/og.png', // Path to your image you placed in the 'static' folder
+    twitterUsername: '@riteshp2000',
+  },
   plugins: [
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Ritesh Patil: Portfolio',
+        short_name: 'Ritesh patil',
+        start_url: '/',
+        background_color: '#0E151C',
+        theme_color: '#FF0A78',
+        display: 'standalone',
+        icon: 'src/images/icon.png',
+        crossOrigin: `use-credentials`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 
+// Libraries
+import {Helmet} from 'react-helmet';
+
 // Components/Configuration
 import {ThemeProvider, GlobalStyles} from '../theming';
 
 function App({children}: {children: React.ReactNode}): JSX.Element {
   return (
-    <ThemeProvider>
-      <GlobalStyles />
-      {children}
-    </ThemeProvider>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Ritesh Patil</title>
+        <link rel="canonical" href="http://riteshpatil.com" />
+      </Helmet>
+
+      <ThemeProvider>
+        <GlobalStyles />
+        {children}
+      </ThemeProvider>
+    </>
   );
 }
 
