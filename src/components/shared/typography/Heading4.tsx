b955c55e75de235952e0d25fc60200df903bdb41
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-16 19:27:36 +0530

    feat: create typography components

---
+++ src/components/shared/typography/Heading4.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Libraries
+import styled from 'styled-components';
+
+export default styled.h4`
+  color: var(--color-text-primary);
+  font-weight: var(--font-weight-regular);
+  font-family: var(--font-family);
+  font-style: normal;
+  font-size: 1.25rem;
+  line-height: 1.6;
+`;
---
+++ src/components/shared/typography/Para1.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Libraries
+import styled from 'styled-components';
+
+export default styled.p`
+  color: var(--color-text-primary);
+  font-weight: var(--font-weight-regular);
+  font-family: var(--font-family);
+  font-style: normal;
+  font-size: 1.125rem;
+  line-height: 1.6;
+`;
