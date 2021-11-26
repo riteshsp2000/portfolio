3993e9e8daedbe5b7a8b49fece53077652a397f1
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-26 16:28:52 +0530

    feat: create flexbox component

---
+++ src/components/atoms/Flexbox.tsx/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import styled from 'styled-components';
+
+interface FlexboxProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  justifyStart?: boolean;
+  justifyCenter?: boolean;
+  justifyEnd?: boolean;
+  justifyAround?: boolean;
+  justifyBetween?: boolean;
+  justifyEvenly?: boolean;
+  alignStart?: boolean;
+  alignCenter?: boolean;
+  alignEnd?: boolean;
+  flexColumn?: boolean;
+  flexWrap?: boolean;
+}
+
+export default styled.div<FlexboxProps>`
+  display: flex;
+  flex-direction: ${({flexColumn}) => (flexColumn ? 'column' : 'row')};
+
+  justify-content: flex-start;
+  justify-content: ${({justifyStart}) => justifyStart && 'flex-start'};
+  justify-content: ${({justifyCenter}) => justifyCenter && 'center'};
+  justify-content: ${({justifyEnd}) => justifyEnd && 'flex-end'};
+  justify-content: ${({justifyAround}) => justifyAround && 'flex-start'};
+  justify-content: ${({justifyBetween}) => justifyBetween && 'flex-start'};
+  justify-content: ${({justifyEvenly}) => justifyEvenly && 'flex-start'};
+
+  align-items: flex-start;
+  align-items: ${({alignStart}) => alignStart && 'flex-start'};
+  align-items: ${({alignCenter}) => alignCenter && 'center'};
+  align-items: ${({alignEnd}) => alignEnd && 'flex-end'};
+
+  flex-wrap: ${({flexWrap}) => flexWrap && 'wrap'};
+`;
