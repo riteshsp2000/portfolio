e945f92289d7e4beb29879ac3c683de1efa6e2c7
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-26 15:46:07 +0530

    fix: minor changes

--- src/components/projects/FeaturedProject.tsx
+++ src/components/projects/FeaturedProject.tsx
@@ -4,7 +4,31 @@ import React from 'react';
 import styled from 'styled-components';
 
 // Components
-import {Card, Image, H1, P1, P2} from '@components';
+import {Card, Image, H1 as Head1, P1 as Para1, P2} from '@components';
+
+const H1 = styled(Head1)`
+  @media (max-width: 700px) {
+    color: var(--color-text-primary);
+    font-weight: var(--font-weight-bold);
+    font-family: var(--font-family);
+    font-style: normal;
+    font-size: 1.5rem;
+    line-height: 1.6;
+    transition: var(--transition);
+  }
+`;
+
+const P1 = styled(Para1)`
+  @media (max-width: 700px) {
+    color: var(--color-text-primary);
+    font-weight: var(--font-weight-regular);
+    font-family: var(--font-family);
+    font-style: normal;
+    font-size: 1rem;
+    line-height: 1.6;
+    transition: var(--transition);
+  }
+`;
 
 const AlignerContainer = styled.div`
   width: 100%;
@@ -16,35 +40,49 @@ const AlignerContainer = styled.div`
 const Container = styled(Card)`
   width: 80%;
   max-width: var(--max-width);
+  overflow: hidden;
 
   padding: 2rem 4rem;
   margin: 0;
 
   display: grid;
-  grid-template-columns: 1fr 2fr;
-  grid-template-rows: 1.5fr 1fr;
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 50px repeat(3, 100px) 50px;
   grid-gap: 1rem;
+
+  @media (max-width: 700px) {
+    padding: 0.8rem;
+    width: 100%;
+    grid-template-rows: repeat(5, 100px) 50px;
+  }
 `;
 
 const Section1 = styled.div<{type: 'mobile' | 'web'}>`
-  grid-column: 1/2;
-  grid-row: 1/3;
+  grid-column: 1 / span 1;
+  grid-row: 1 / span 5;
   width: 100%;
   height: 100%;
-
-  background: red;
 
   .project-image {
     aspect-ratio: 0.525;
     object-fit: contain;
-    height: 500px;
-    width: auto;
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 700px) {
+    grid-column: 1 / span 3;
+    grid-row: 4 / span 2;
+
+    .project-image {
+      width: 80%;
+    }
   }
 `;
 
 const Section2 = styled.div`
-  grid-column: 2/3;
-  grid-row: 1/2;
+  grid-column: 2 / span 2;
+  grid-row: 1 / span 3;
   width: 100%;
   height: 100%;
 
@@ -53,15 +91,17 @@ const Section2 = styled.div`
   align-items: flex-start;
   justify-content: flex-start;
 
-  background: green;
+  @media (max-width: 700px) {
+    grid-column: 1 / span 3;
+    grid-row: 1 / span 3;
+  }
 `;
 
 const Section3 = styled.div`
-  grid-column: 2/3;
-  grid-row: 2/3;
+  grid-column: 2 / span 2;
+  grid-row: 4 / span 2;
   width: 100%;
   height: 100%;
-
   padding-top: 2rem;
 
   display: flex;
@@ -69,7 +109,11 @@ const Section3 = styled.div`
   align-items: flex-start;
   justify-content: flex-start;
 
-  background: blue;
+  @media (max-width: 700px) {
+    padding-top: 0.5rem;
+    grid-column: 1 / span 3;
+    grid-row: 6 / span 2;
+  }
 `;
 
 const TechContainer = styled.div`
@@ -105,9 +149,6 @@ const FeaturedProject: React.FC<FeaturedProjectProps> = ({
             style={{
               aspectRatio: '0.525',
               objectFit: 'contain',
-              // width: type === 'mobile' ? '50%' : '90%',
-              height: '500px',
-              width: 'auto',
             }}
             className="project-image"
             alt="boutiques project"
