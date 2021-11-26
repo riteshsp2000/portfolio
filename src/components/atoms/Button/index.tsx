102db89e3f6830605fa5816e78cfda73964744e1
Ritesh Patil <riteshsp2000@gmail.com>
2021-11-26 16:53:03 +0530

    feat: create atoms - part 1

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export default styled.button`
+  background: var(--color-background-primary);
+  border: 1px solid var(--color-text-primary);
+
+  border-radius: 50;
+  width: auto;
+  min-width: 100px;
+  height: auto;
+
+  color: var(--color-text-primary);
+  font-weight: var(--font-weight-regular);
+  font-family: var(--font-family);
+  font-style: normal;
+  font-size: 1.125rem;
+  line-height: 1.6;
+  transition: var(--transition);
+
+  &:disabled {
+    background: var(--color-background-secondary);
+    border: 1px solid var(--color-background-secondary);
+    color: var(--color-background-secondary);
+  }
+
+  &:hover,
+  &:focus {
+    background: var(--color-text-primary);
+    color: var(--color-background-primary);
+  }
+`;
