09bd64f9733e973b0fddbc745b340c4cb7c2ac8c
Ritesh Patil <riteshsp2000@gmail.com>
2021-12-09 15:05:38 +0530

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,86 @@
-/* eslint-disable arrow-body-style */
 import React from 'react';
 
+// Libraries
+import styled from 'styled-components';
+
+// Components
+import {
+  Flexbox,
+  FancyText,
+  P3,
+  H3,
+  WorkExperience,
+  SectionTitle,
+} from '@components';
+
+import {BREAKPOINTS} from '@theme';
+
+const SectionContainer = styled(Flexbox)`
+  width: 100%;
+  height: calc(100vh - 90px);
+`;
+
+const ImgContainer = styled(Flexbox)`
+  width: 35%;
+  height: 260px;
+
+  @media ${BREAKPOINTS.md} {
+    width: 100%;
+  }
+
+  .image {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+  }
+`;
+
+const Fancy = styled(FancyText)`
+  font-size: 4.5rem;
+  text-align: center;
+
+  @media ${BREAKPOINTS.md} {
+    font-size: 2.5rem;
+  }
+`;
+
+const Subtext = styled(H3)`
+  color: var(--color-text-tertiary);
+  margin-bottom: 2rem;
+  width: 45%;
+  text-align: center;
+  font-weight: 400;
+  font-size: 1.3rem;
+  line-height: 1.4;
+
+  @media ${BREAKPOINTS.md} {
+    width: 100%;
+  }
+`;
+
 const Home: React.FC = () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
+  <>
+    <SectionContainer flexColumn alignCenter justifyCenter>
+      <ImgContainer>
+        <img
+          src="https://res.cloudinary.com/riteshp2000/image/upload/v1639041087/portfolio/v2/assets/animation_500_kwyqqls2_qsxyrf.gif"
+          alt="Ritesh Patil - Software Developer"
+          className="image"
+        />
+      </ImgContainer>
+
+      <Fancy>Hi, I'm Ritesh Patil</Fancy>
+
+      <Subtext>
+        A Software developer specialising in building performant frontend
+        applications
+      </Subtext>
+    </SectionContainer>
+
+    <SectionTitle title="Work" highlight="Experience" />
+    <WorkExperience />
+  </>
 );
 
 export default Home;
