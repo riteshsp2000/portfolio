752911cbddba2467d5e184ba763bdbd872bbcab2
Ritesh Patil <riteshsp2000@gmail.com>
2022-02-27 15:57:40 +0530

    Update index.tsx

--- src/components/articles/FrameGenerator/index.tsx
+++ src/components/articles/FrameGenerator/index.tsx
@@ -1,25 +1,11 @@
 import React, {useEffect} from 'react';
 
 const FrameGenerator = ({}) => {
-  // useEffect(() => {
-  //   function iframeLoaded() {
-  //     const iFrameID = document.getElementById('idIframe');
-  //     if (iFrameID) {
-  //       const {width} = iFrameID.getBoundingClientRect();
-  //       iFrameID.style.height = '';
-  //       iFrameID.style.height = `${(9 * width) / 16}px`;
-  //     }
-  //   }
-
-  //   iframeLoaded();
-  // }, []);
-
   return (
     <iframe
       width="100%"
       height="600"
-      // src="https://gracious-volhard-22bfe6.netlify.app/frame"
-      src="http://localhost:8001/frame"
+      src="https://gracious-volhard-22bfe6.netlify.app/frame"
       title="React Konva Frame Generator"
       frameBorder="0"
       id="idIframe"
