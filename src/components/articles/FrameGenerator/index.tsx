58a2643d63430ff8fa45d5d238ec61fd26217540
Ritesh Patil <riteshsp2000@gmail.com>
2022-02-27 16:15:56 +0530

    add frame generator component in the article (#32)
    * add frame generator component in the article
    
    * Update index.tsx
    
    * Update blog-1.mdx

---
+++ src/components/articles/FrameGenerator/index.tsx
@@ -0,0 +1,16 @@
+import React, {useEffect} from 'react';
+
+const FrameGenerator = ({}) => {
+  return (
+    <iframe
+      width="100%"
+      height="600"
+      src="https://gracious-volhard-22bfe6.netlify.app/frame"
+      title="React Konva Frame Generator"
+      frameBorder="0"
+      id="idIframe"
+    />
+  );
+};
+
+export default FrameGenerator;
